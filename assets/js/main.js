c09eeab608e32113c77859824d5183a305fcac00
Lior Yitzhak <dev@lior.world>
2026-01-08 17:30:02 +0200

    Fix whatsapp button on desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
-const btnWhatsAppChat = document.querySelector(".btn-whatsapp-chat");
+const btnWhatsAppChat = document.querySelectorAll('.btn-whatsapp-chat');
 
-const phone = "972505220276";
-const waMessage = `שלום אמנון, הגעתי אליך דרך האתר של כרמל הדברות.
+const phone = '972505220276';
+const waMessage = `שלום אמנון, הגעתי אליך דרך האתר של כרמל הדברות. 
 אשמח לקבל פרטים נוספים לגבי השירות שלכם.`;
 
 const btnWhatsAppUrl = `https://wa.me/${phone}?text=${encodeURIComponent(waMessage)}`;
-btnWhatsAppChat.href = btnWhatsAppUrl;
+
+btnWhatsAppChat.forEach((btn) => {
+  btn.href = btnWhatsAppUrl;
+});
